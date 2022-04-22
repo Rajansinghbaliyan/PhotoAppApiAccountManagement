f2d3d6141d430fbefddcfc445fda3af6aedbb7b3
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-22 08:13:16 +0530

    feat: added a testing controller

--- src/main/kotlin/io/cherrytechnologies/photoappapiaccountmanagement/web/controllers/AccountManagementController.kt
+++ src/main/kotlin/io/cherrytechnologies/photoappapiaccountmanagement/web/controllers/AccountManagementController.kt
@@ -1,14 +1,18 @@
 package io.cherrytechnologies.photoappapiaccountmanagement.web.controllers
 
+import io.cherrytechnologies.photoappapiaccountmanagement.util.logInfo
 import io.cherrytechnologies.photoappapiaccountmanagement.util.responseOk
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Logger
 
 @RestController
 @RequestMapping("/v1/api/account-management")
 class AccountManagementController {
 
+    val log: Logger = Logger.getLogger(AccountManagementController::class.toString())
+
     @GetMapping("/hello")
-    fun greeting() = "Hello form account management".responseOk()
+    fun greeting() = "Hello form account management".responseOk().logInfo(log,"GET: account-management")
 }
